0570ee34e1295a13e3db110d5cdbdd4cb3946ebb
giulianoarend <giuliano.arend@gmail.com>
2014-01-24 17:26:43 +0100

    V1

---
+++ sat_v1/sudoku_v1/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Unmonprojet.h"
+#include "affichage.h"
+#include "regles.h"
+#include "satgrille.h"
+#include "decodage.h"*
+
+/*
+ *
+ */
+
+
+
+int main(int argc, char** argv) {
+
+    printf("############################################################\n");
+    printf("###################    SUDOKU AVEC SAT    ##################\n");
+    printf("############################################################\n");
+    int dimension = 0;
+    while (dimension != 4 && dimension != 9) {
+	printf("\nSaisissez la largeur désirée (4 ou 9): ");
+	scanf("%i", &dimension);
+	if (dimension != 4 && dimension != 9) {
+	    printf("Erreur : les deux seules largeurs possibles sont 4 ou 9.");
+	}
+    }
+    Grille G;
+    G = creer_grille(dimension);
+    raz_grille(G, dimension);
+    saisir_grille(G, dimension);
+    resoudre(G,dimension);
+    return 0;
+}
+
